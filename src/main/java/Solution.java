f39f64fa5f77e44bb9d2295610ef62f623a6611b
alek <wait82985@gmail.com>
2023-12-18 19:28:52 +0600

    problem solved

---
+++ src/main/java/Solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int maxProductDifference(int[] nums) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max1 = -1;
+        int max2 = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(min1 > nums[i]){
+                min2 = min1;
+                min1 = nums[i];
+            }else if(min2 > nums[i]){
+                min2 = nums[i];
+            }
+            if(max1 < nums[i]){
+                max2 = max1;
+                max1 = nums[i];
+            }else if(max2 < nums[i]){
+                max2 = nums[i];
+            }
+        }
+
+        return max1 * max2 - min1 * min2;
+    }
+}
